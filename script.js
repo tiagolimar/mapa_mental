ab2a7ba45ff88f1917afda222237f88597252f62
tiagolimar <tiagolima.r2@gmail.com>
2023-08-12 13:11:11 -0300

    aprendendo konvaJS

--- script.js
+++ script.js
@@ -1,147 +1,153 @@
-let count = 0;
+// Configuração do stage e layer
+const stage = new Konva.Stage({
+    container: 'map-container',
+    width: window.innerWidth,
+    height: window.innerHeight
+});
+const connections = []; // Array de conexões
+const layer = new Konva.Layer();
+stage.add(layer);
+let connectingNodeId = null; // Armazenar o ID do campo de texto de partida da conexão
 
+function onNodeClick(nodeId) {
+    if (connectingNodeId === null) {
+        connectingNodeId = nodeId;
+    } else {
+        if (connectingNodeId !== nodeId) {
+            // Criar uma conexão entre os campos
+            const newConnection = { from: connectingNodeId, to: nodeId };
+            connections.push(newConnection);
+
+            // Chamar função para desenhar as conexões
+            drawConnections();
+
+            connectingNodeId = null; // Reiniciar para permitir futuras conexões
+        }
+    }
+}
+function drawConnections() {
+    // Limpar todas as conexões existentes
+    try {
+        layer.find('.connection').destroy();
+    } catch (error) {}
+
+    // Desenhar as novas conexões
+    for (const connection of connections) {
+        const fromNode = textFields.find(node => node.id() === connection.from);
+        const toNode = textFields.find(node => node.id() === connection.to);
+
+        if (fromNode && toNode) {
+            const connector = new Konva.Line({
+                points: [fromNode.x(), fromNode.y(), toNode.x(), toNode.y()],
+                stroke: 'black',
+                strokeWidth: 2,
+                lineCap: 'round',
+                lineJoin: 'round',
+                id: 'connection'
+            });
+
+            layer.add(connector);
+        }
+    }
+
+    layer.draw();
+}
+
+// Função para criar campo de texto redimensionável
+function createResizableText(x, y) {
+    const textGroup = new Konva.Group({
+        x: x,
+        y: y,
+        draggable: true,
+        width: 200,
+        height: 100
+    });
+
+    const textBackground = new Konva.Rect({
+        width: 200,
+        height: 100,
+        fill: '#f0f0f0',
+        cornerRadius: 10
+    });
+
+    const textNode = new Konva.Text({
+        text: 'Digite aqui',
+        fontSize: 18,
+        padding: 10,
+        align: 'center',
+        verticalAlign: 'middle',
+        wrap: 'word'
+    });
+
+    textNode.on('dblclick', () => {
+        // Hide text node and show textarea
+        textNode.hide();
+        layer.draw();
+
+        const textPosition = textNode.getAbsolutePosition();
+
+        const textarea = document.createElement('textarea');
+        textarea.style.position = 'absolute';
+        textarea.style.top = textPosition.y + 'px';
+        textarea.style.left = textPosition.x + 'px';
+        textarea.style.width = textBackground.width() + 'px';
+        textarea.style.height = textBackground.height() + 'px';
+        textarea.value = textNode.text();
+        document.body.appendChild(textarea);
+
+        textarea.addEventListener('input', () => {
+            textNode.text(textarea.value);
+            layer.draw();
+        });
+
+        textarea.addEventListener('blur', () => {
+            textNode.text(textarea.value);
+            layer.draw();
+            textarea.remove();
+            textNode.show();
+        });
+
+        textarea.focus();
+    });
+
+    textGroup.on('click', () => {
+        onNodeClick(textGroup.id());
+    });
+
+    textGroup.add(textBackground);
+    textGroup.add(textNode);
+    layer.add(textGroup);
+    layer.draw();
+
+    // Adicionar redimensionamento
+    textGroup.on('transform', () => {
+        const scaleX = textGroup.scaleX();
+        const scaleY = textGroup.scaleY();
+
+        textBackground.width(textBackground.width() * scaleX);
+        textBackground.height(textBackground.height() * scaleY);
+
+        textNode.width(textBackground.width() - 20);
+        textNode.height(textBackground.height() - 20);
+
+        textGroup.scaleX(1);
+        textGroup.scaleY(1);
+        layer.draw();
+    });
+
+    textGroup.on('dragmove', () => {
+        textBackground.width(textNode.width() + 20);
+        textBackground.height(textNode.height() + 20);
+    });
+}
+
+let textFields = [];
+// Função para adicionar novo campo de texto
 function addRectangle() {
-    const container = document.getElementById("container");
+    const x = Math.random() * stage.width();
+    const y = Math.random() * stage.height();
 
-    const elementContainer = document.createElement("div");
-    elementContainer.className = `container${count} main`;
-
-    elementContainer.style.width = "100px";
-    elementContainer.style.height = "100px";
-
-    const rectangle = createRectangle();
-    const topCircle = createCircle("top");
-    const rightCircle = createCircle("right");
-    const bottomCircle = createCircle("bottom");
-    const leftCircle = createCircle("left");
-
-    count++;
-
-    positionCircle(elementContainer, topCircle, "50%", "0");
-    positionCircle(elementContainer, rightCircle, "100%", "50%");
-    positionCircle(elementContainer, bottomCircle, "50%", "100%");
-    positionCircle(elementContainer, leftCircle, "0", "50%");
-
-    elementContainer.appendChild(rectangle);
-    elementContainer.appendChild(topCircle);
-    elementContainer.appendChild(rightCircle);
-    elementContainer.appendChild(bottomCircle);
-    elementContainer.appendChild(leftCircle);
-
-    elementContainer.addEventListener("mousedown", startDrag);
-
-    container.appendChild(elementContainer);
-}
-
-function createRectangle() {
-    const rectangle = document.createElement("textarea");
-    rectangle.className = `container${count} rectangle`;
-    rectangle.style.width = "200px";
-    rectangle.style.height = "30px";
-    rectangle.style.top = "0";
-    rectangle.style.left = "0";
-    return rectangle;
-}
-
-function createCircle(position) {
-    const circle = document.createElement("div");
-    circle.style.width = "10px";
-    circle.style.height = "10px";
-    circle.style.borderRadius = "50%";
-    circle.style.backgroundColor = "gray";
-    circle.style.position = "absolute";
-    circle.className = `container${count} circle ${position}`;
-    return circle;
-}
-
-function positionCircle(rectangle, circle, x, y) {
-    const rectCoords = rectangle.getBoundingClientRect();
-    const rectX = rectCoords.left;
-    const rectY = rectCoords.top;
-    const rectWidth = rectCoords.width;
-    const rectHeight = rectCoords.height;
-
-    let circleX = x;
-    let circleY = y;
-
-    if (x == "50%") {
-        circleX = rectX + rectWidth / 2;
-    } else if (x == "100%") {
-        circleX = rectX + rectWidth;
-    }
-
-    if (y == "50%") {
-        circleY = rectY + rectHeight / 2;
-    } else if (y == "100%") {
-        circleY = rectY + rectHeight;
-    }
-
-    circle.style.left = circleX + "px";
-    circle.style.top = circleY + "px";
-    circle.style.transform = "translate(-50%, -50%)";
-}
-
-function findClassByString(element, searchString) {
-    if (!element || typeof element.classList !== "object") {
-        return "";
-    }
-
-    const classes = Array.from(element.classList);
-    const foundClass = classes.find((className) =>
-        className.includes(searchString)
-    );
-
-    return foundClass || "";
-}
-
-function startDrag(e) {
-    if (findClassByString(e.target, "rectangle")) {
-        const elementContainer = e.target;
-        const container = document.getElementById("container");
-        const id = findClassByString(elementContainer, "container");
-        const elementCoords = elementContainer.getBoundingClientRect();
-        const offsetX = e.clientX - elementCoords.left;
-        const offsetY = e.clientY - elementCoords.top;
-
-        topCircle = document.querySelector(`.${id}.top`);
-        rightCircle = document.querySelector(`.${id} .right`);
-        bottomCircle = document.querySelector(`.${id} .bottom`);
-        leftCircle = document.querySelector(`.${id} .left`);
-
-        document.onmousemove = function (e) {
-            const x = e.clientX - offsetX;
-            const y = e.clientY - offsetY;
-
-            const containerWidth = container.clientWidth;
-            const containerHeight = container.clientHeight;
-
-            elementContainer.style.left =
-                Math.min(Math.max(0, x), containerWidth - elementCoords.width) +
-                "px";
-            elementContainer.style.top =
-                Math.min(
-                    Math.max(0, y),
-                    containerHeight - elementCoords.height
-                ) + "px";
-
-            const circleCoords = elementContainer.getBoundingClientRect();
-            const circleX = circleCoords.left + circleCoords.width / 2;
-            const circleY = circleCoords.top + circleCoords.height / 2;
-
-            updateCirclePosition(topCircle, circleX, circleCoords.top);
-            updateCirclePosition(rightCircle, circleCoords.right, circleY);
-            updateCirclePosition(bottomCircle, circleX, circleCoords.bottom);
-            updateCirclePosition(leftCircle, circleCoords.left, circleY);
-        };
-    }
-
-    document.onmouseup = function () {
-        document.onmousemove = null;
-        document.onmouseup = null;
-    };
-}
-
-function updateCirclePosition(circle, x, y) {
-    circle.style.left = x + "px";
-    circle.style.top = y + "px";
+    const newText = createResizableText(x, y);
+    textFields.push(newText);
+    drawConnections();
 }
